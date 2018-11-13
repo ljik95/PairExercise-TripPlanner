93e31a2d9f02bd1edfde74d76f25519d2e126332
Adam Sisk <adamsisk91@gmail.com>
2018-11-13 15:06:22 -0600

    webpack added, map UI added

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log('created index.js!');
+const mapboxgl = require('mapbox-gl');
+
+mapboxgl.accessToken =
+  'pk.eyJ1IjoiY2FsYW1pdHlhZGFtIiwiYSI6ImNqb2c3d2g2YjBjY2kzcXBuaHQ2Nm01aWwifQ.uZdG9AyEPK2g40QG-u2D5Q';
+
+const map = new mapboxgl.Map({
+  container: 'map',
+  center: [-74.009, 40.705], // FullStack NY coordinates; alternatively, use [-87.6354, 41.8885] for Chicago
+  zoom: 12, // starting zoom
+  style: 'mapbox://styles/mapbox/streets-v10', // mapbox has lots of different map styles available.
+});
